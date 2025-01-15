d7d96c7d994c3e8eb837f6cdd3c74b0c2632dada
AshrafSaf1 <115882371+AshrafSaf1@users.noreply.github.com>
2025-01-15 04:24:27 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// let bg = document.getElementById("navbar");
+// let serv = document.getElementById("services");
+
+// // let c = document.getElementById("nv1");
+
+// window.onscroll = function () {
+//   if (this.scrollY >= 900) {
+//     bg.style.backgroundColor = "white";
+//     // c.style.color = "black";
+//   } else {
+//     bg.style.backgroundColor = "rgba(19, 19, 22, 0.5)";
+//     // c.style.color = "white";
+//   }
+// };
